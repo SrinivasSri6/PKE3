fa5432cfc81a0e588c096e6876869701c28470fd
Srinivas <srinivas@Srinivas.local>
2023-09-29 17:46:19 +0530

    commit1

--- home/animation.js
+++ home/animation.js
@@ -42,10 +42,12 @@ function test() {
             console.log(`Image loaded: ${img.src}`);
             console.log(`Image loaded: ${loadedImages}/${totalImages}`);
   
-            if (loadedImages === totalImages) {
-              console.log('All images loaded. Redirecting...');
-              resolve(true);
-            }
+            // if (loadedImages === totalImages) {
+            //   console.log('All images loaded. Redirecting...');
+            //   resolve(true);
+            // }
+
+             setTimeout(function() {resolve(true)}, 10000);
           });
   
           img.addEventListener('error', () => {
